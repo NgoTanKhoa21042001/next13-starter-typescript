323f0288d4b6a773d69ad4c06ef16181b2c651c8
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-08-16 16:55:32 +0700

    useSWR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,28 @@
 import Link from "next/link";
 import "@/styles/app.css";
 import { useEffect } from "react";
-
+import useSWR from "swr";
 export default function Home() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("http://localhost:8000/blogs");
-      const data = await res.json();
-      console.log(data);
-    };
-    fetchData();
-  }, []);
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/blogs",
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch("http://localhost:8000/blogs");
+  //     const data = await res.json();
+  //     console.log(data);
+  //   };
+  //   fetchData();
+  // }, []);
+  console.log(data);
 
   return (
     <main>
